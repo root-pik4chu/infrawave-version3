fb9e548af8676ccbee457d097076f147915af2a0
root <68701068+root-pik4chu@users.noreply.github.com>
2025-05-03 17:26:11 +0530

    maybe i solve this err

--- src/pages/Components/Preloader/index.jsx
+++ src/pages/Components/Preloader/index.jsx
@@ -2,7 +2,7 @@
 import styles from '../Preloader/styles.module.scss';
 import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-import { opacity, slideUp } from './anim';
+import { opacity, slideUp } from '../../../../src/Layout/Curve/anim';
 
 const words = ["Infrawave Solutions", " "," ", "Web Dev " ,"AI Ml" , "BlockChain" ]
 
