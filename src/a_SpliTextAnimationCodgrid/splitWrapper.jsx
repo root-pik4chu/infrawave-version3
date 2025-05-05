8589cb65c7640fa3c48ec85a74555d699d8515c6
root <68701068+root-pik4chu@users.noreply.github.com>
2025-05-06 03:05:20 +0530

    added text effect finally

---
+++ src/a_SpliTextAnimationCodgrid/splitWrapper.jsx
@@ -0,0 +1,105 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(SplitText, ScrollTrigger);
+
+export default function SplitTextAnimationWrapper({
+  children,
+  animateOnScroll = true,
+  delay = 0.1,
+}) {
+  const containerRef = useRef(null);
+  const elementRef = useRef([]);
+  const splitTextRef = useRef([]);
+  const lines = useRef([]);
+
+  useGSAP(
+    () => {
+      if (!containerRef.current) return;
+      splitTextRef.current = [];
+      elementRef.current = [];
+      lines.current = [];
+
+      let elements = [];
+      if (containerRef.current.hasAttribute("data-copy-wrapper")) {
+        elements = Array.from(containerRef.current.children);
+      } else {
+        elements = [containerRef.current];
+      }
+      // !-----------------------------
+      elements.forEach((element) => {
+        elementRef.current.push(element);
+
+        const split = SplitText.create(element, {
+          type: "lines",
+          mask: "lines",
+          lineClass: "line++",
+        });
+        splitTextRef.current.push(split);
+
+        const computedStyle = window.getComputedStyle(element);
+        const TextIndent = computedStyle.textIndent;
+
+        if (TextIndent && TextIndent !== "0px") {
+          if (split.lines.length > 0) {
+            split.lines[0].style.paddingLeft = TextIndent;
+          }
+          element.style.textIndent = "0";
+        }
+        lines.current.push(...split.lines);
+      });
+      gsap.set(lines.current, { y: "100%" });
+
+      const animationProp = {
+        y: 0,
+        duration: 2,
+        ease: "power4.out",
+        stagger: 0.1,
+        delay: delay,
+      };
+
+      if (animateOnScroll) {
+        gsap.to(lines.current, {
+          ...animationProp,
+          scrollTrigger: {
+            trigger: containerRef.current,
+            start: "top 75%",
+            once: true,
+            markers:true,
+          },
+        });
+      } else {
+        gsap.to(lines.current, {
+          ...animationProp,
+          delay: delay,
+        });
+      }
+
+      return () => {
+        splitTextRef.current.forEach((split) => {
+          if (split) {
+            split.revert();
+          }
+        });
+      };
+    },
+    {
+      scope: containerRef,
+      dependencies: [animateOnScroll, delay],
+    }
+  );
+
+  if(React.Children.count(children)===1){
+  return React.cloneElement(children, { ref: containerRef, });
+
+  }
+  return (
+   <div className="" ref={containerRef} data-copy-wrapper>
+      {children}
+   </div>
+  )
+}
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,10 @@
+"use client"
 import React,{useEffect} from 'react';
 import Layout from "../Layout/Curve"
 
 import LocomotiveScroll from 'locomotive-scroll';
 import 'locomotive-scroll/dist/locomotive-scroll.css';
-
+import SplitWrapper from "../a_SpliTextAnimationCodgrid/splitWrapper"
 const About = () => {
 
   useEffect(() => {
@@ -21,15 +22,28 @@ const About = () => {
     <>
     
     <Layout >
-      <div className="w-full h-[5vw] bg-red-950"></div>
+      <div className="w-full h-[5vw] bg-zinc-50">
+        
+<SplitWrapper>
+  <h4 className=' h-[30vh] bg-red-300'  >
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam nulla corrupti eum consequuntur culpa! Dolor doloribus ipsam a, consequatur recusandae earum aliquam. Maxime error omnis eum exercitationem provident repellat, neque aut nemo quas fuga tempore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.m provident repellat, neque aut nemo quas fuga tempore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.m provident repellat, neque aut nemo quas fuga tempore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.m provident repellat, neque aut nemo quas fuga tempore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.m provident repellat, neque aut nemo quas fuga tempore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.pore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.</h4>
+  </SplitWrapper>
+      </div>
+<div className="w-full h-screen bg-red-900"><h2>hii</h2></div>
+
 <div className="w-full h-screen bg-red-900"><h2>hii</h2>
 
-
-
-
+<SplitWrapper>
+  <h4 >
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam nulla corrupti eum consequuntur culpa! Dolor doloribus ipsam a, consequatur recusandae earum aliquam. Maxime error omnis eum exercitationem provident repellat, neque aut nemo quas fuga tempore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.</h4>
+  </SplitWrapper><SplitWrapper>
+  <h4>
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam nulla corrupti eum consequuntur culpa! Dolor doloribus ipsam a, consequatur recusandae earum aliquam. Maxime error omnis eum exercitationem provident repellat, neque aut nemo quas fuga tempore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.</h4>
+  </SplitWrapper><SplitWrapper>
+  <h4>
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam nulla corrupti eum consequuntur culpa! Dolor doloribus ipsam a, consequatur recusandae earum aliquam. Maxime error omnis eum exercitationem provident repellat, neque aut nemo quas fuga tempore modi quaerat similique porro qui blanditiis temporibus nostrum unde quos dolorem? Asperiores et delectus accusamus.</h4>
+  </SplitWrapper>
 </div>
-<div className="w-full h-screen bg-red-900"><h2>hii</h2></div>
-<div className="w-full h-screen bg-red-900"><h2>hii</h2></div>
 
     </Layout>
     </>
