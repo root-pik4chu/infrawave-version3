124e2ce77f23bd0a5d9515fbf44f094e030297e3
root <68701068+root-pik4chu@users.noreply.github.com>
2025-05-07 12:13:13 +0530

    now its working

--- src/pages/Components/landing/AboutSection.jsx
+++ src/pages/Components/landing/AboutSection.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function AboutSection(){
   return (
-    <div className="w-full h-screen bg-[#FF4E27] flex">
+    <div className="w-full h-screen bg-[#FF4E27] flex overflow-hidden">
      <h1>root pikachu</h1>
      <h1>root pikachu</h1>
      <h1>root pikachu</h1>
