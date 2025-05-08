7030c6d08c50d53896fd2bc93dc945b4225f4b8d
root <68701068+root-pik4chu@users.noreply.github.com>
2025-05-08 18:06:05 +0530

    css problem

--- src/pages/Components/landing/Swipper/SwipperData.jsx
+++ src/pages/Components/landing/Swipper/SwipperData.jsx
@@ -3,7 +3,7 @@ import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
-import "../Swipper/SwipperStyles.css";
+import "../../../../extracss/swiper.css"
 import { Navigation } from "swiper/modules";
 
 export default function SwipperData() {
